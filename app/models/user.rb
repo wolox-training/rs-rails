c5e839d8fba93675c732005711c9f53c908047bb
jicardu <ricardosantamaria@outlook.com>
2019-11-25 14:52:05 -0300

    Fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
-class User < ActiveRecord::Base
+# frozen_string_literal: true
+
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
