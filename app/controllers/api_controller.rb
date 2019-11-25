54ee4b0f7dd9bfe2905c52ab34f802b7e7d603c7
jicardu <ricardosantamaria@outlook.com>
2019-11-25 16:09:14 -0300

    Fix offenses

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class ApiController < ActionController::API
-    include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
 end
-  
