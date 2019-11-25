91b971d1d254afa1ec5dddac7266f7056af37e98
jicardu <ricardosantamaria@outlook.com>
2019-11-25 15:50:56 -0300

    Delete some devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable
   include DeviseTokenAuth::Concerns::User
 end
