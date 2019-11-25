a5f385db3f6ac15e651bfb81bc67a7ec48567f2d
jicardu <ricardosantamaria@outlook.com>
2019-11-25 14:45:50 -0300

    Add protect_from_forgery with null session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@
 
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
