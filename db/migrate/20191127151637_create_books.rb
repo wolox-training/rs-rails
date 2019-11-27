4ce59fbca4ed13b366cfb94773f51a0c0774bb77
jicardu <ricardosantamaria@outlook.com>
2019-11-27 12:19:32 -0300

    Create model book

---
+++ db/migrate/20191127151637_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :genre, :null => false, :default => ""
+      t.string :author, :null => false, :default => ""
+      t.string :image, :null => false, :default => ""
+      t.string :title, :null => false, :default => ""
+      t.string :editor, :null => false, :default => ""
+      t.string :year, :null => false, :default => ""
+      t.timestamps
+    end
+  end
+end
