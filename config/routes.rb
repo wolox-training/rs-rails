c8f2f961d0c619bcbd4dadba883f43814f700759
jicardu <ricardosantamaria@outlook.com>
2019-12-11 15:39:05 -0300

    Versionate controller book

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/book/:id', to: 'books#show'
-  get '/books', to: 'books#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :show]
+    end
+  end
 end
