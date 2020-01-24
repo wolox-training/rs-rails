a511f061de62e603bb4a6325803ba08009e41d97
jicardu <ricardosantamaria@outlook.com>
2020-01-24 12:01:47 -0500

    Fix offenses

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,8 +1,9 @@
-class RentMailer < ApplicationMailer
-    def new_rent_email
-        @rent = params[:rent]
-        @user = params[:user]
-        mail(to: @user.email, subject: "New rent created")
-    end
-end
+# frozen_string_literal: true
 
+class RentMailer < ApplicationMailer
+  def new_rent_email
+    @rent = params[:rent]
+    @user = params[:user]
+    mail(to: @user.email, subject: 'New rent created')
+  end
+end
